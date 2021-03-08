3753195fb0b9101435253c645a0e74b851ba2dd7
umang-sinha <umangsinha12@gmail.com>
2021-03-08 15:06:50 +0530

    added introductory greeting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import source as source
 import speech_recognition as sr
 import pyttsx3
 
@@ -5,17 +6,18 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.say("Hey Umang! What can I do for you?")
+engine.say("Hi! I am Alexa, your virtual assistant. What can I do for you?")
 engine.runAndWait()
 
 try:
     with sr.Microphone() as source:
-        print('Listening 👂...')
-        #for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        # for index, name in enumerate(sr.Microphone.list_microphone_names()):
         #    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+        print('Listening 👂...')
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
         command = command.lower()
+        print(command)
         if 'alexa' in command:
             print(command)
         else:
