a97e328fb68a3673c7ffc3f23e2aaf101cc1ab25
umang-sinha <umangsinha12@gmail.com>
2021-03-08 14:45:08 +0530

    added text to speech and changed the voice to a female voice

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say("Hey Umang! What can I do for you?")
+engine.runAndWait()
 
 try:
     with sr.Microphone() as source:
