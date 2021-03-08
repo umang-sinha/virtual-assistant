74f5aab1c1b3eb99fdffa4c05ee83070db0893bf
umang-sinha <umangsinha12@gmail.com>
2021-03-08 15:29:01 +0530

    created functions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import source as source
 import speech_recognition as sr
 import pyttsx3
 
@@ -6,17 +5,32 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.say("Hi! I am Alexa, your virtual assistant. What can I do for you?")
-engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        # for index, name in enumerate(sr.Microphone.list_microphone_names()):
-        #    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
-        print('Listening 👂...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        print(command)
-except:
-    print('Something went wrong')
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            # for index, name in enumerate(sr.Microphone.list_microphone_names()):
+            #    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+            print('Listening 👂...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            talk(command)
+    except:
+        print('Something went wrong')
+        return command
+
+
+def introductory_greeting():
+    engine.say("Hi! I am Alexa, your virtual assistant. What can I do for you?")
+    engine.runAndWait()
+
+
+introductory_greeting()
+take_command()
